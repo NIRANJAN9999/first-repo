2fcdc6d64c81ecc7d088d91cd459aa503704791f
Mike <niranjansharabani22@gmail.com>
2024-08-26 19:02:23 +0530

    new rating feature

--- test_pythonfile.py
+++ test_pythonfile.py
@@ -13,7 +13,17 @@
 
 
 
-def greet ():
+def greet():
     pname = input("Please Enter Your Name ")
     print(f"Hello,{pname}! thanks for choosing us")
+## writing a new feature for version 3
+# this  is third update
+def rating():
+    rating = int(input("please rate from 1-5 \n please enter your input in numbers"))
+    if rating > 5:
+        print("we love your rating :)")
+    else:
+        print("thanks for rating us.",rating)
+
 greet()
+rating()
