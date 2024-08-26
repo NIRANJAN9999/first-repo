f9c0ce76a6df1a28ef67e298f49cf7fd1f0a6415
Mike <niranjansharabani22@gmail.com>
2024-08-26 18:35:39 +0530

    this is v2

--- test_pythonfile.py
+++ test_pythonfile.py
@@ -2,5 +2,18 @@
 # Greetings Program
 
 #greetings
-greet = input("Hello, may i know your name..")
-print("Hello ",greet, "!  thanks for choosing us.")
+#greet = input("Hello, may i know your name..")
+#print("Hello ",greet, "!  thanks for choosing us.")
+
+
+# this is my second version
+
+#Greetings Program
+
+
+
+
+def greet ():
+    pname = input("Please Enter Your Name ")
+    print(f"Hello,{pname}! thanks for choosing us")
+greet()
