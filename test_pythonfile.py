c533822afe1636786b658fcb486ded3ab68eeecd
Mike <niranjansharabani22@gmail.com>
2024-08-26 19:33:32 +0530

    this is a test feature of elif

--- test_pythonfile.py
+++ test_pythonfile.py
@@ -22,8 +22,12 @@ def rating():
     rating = int(input("please rate from 1-5 \n please enter your input in numbers"))
     if rating > 5:
         print("we love your rating :)")
+    #this is new feature of rating 
+    #this is pushing into newbranch
+    elif rating >2:
+        print("thanks for your rating. ")
     else:
-        print("thanks for rating us.",rating)
+        print("we are very sorry for your experience",rating)
 
 greet()
 rating()
