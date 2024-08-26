8500b8835e20974a47120e296553fb09938852de
Niranjan_Sharabani <niranjansharabani22@gmail.com>
2024-08-27 01:21:35 +0530

    i made some changes

--- test_pythonfile.py
+++ test_pythonfile.py
@@ -13,17 +13,22 @@
 
 
 
-def greet():
-    pname = input("Please Enter Your Name ")
-    print(f"Hello,{pname}! thanks for choosing us")
-## writing a new feature for version 3
-# this  is third update
-def rating():
-    rating = int(input("please rate from 1-5 \n please enter your input in numbers"))
-    if rating > 5:
-        print("we love your rating :)")
-    else:
-        print("thanks for rating us.",rating)
 
-greet()
-rating()
+
+# here i made little changes @some_0_n_e
+def greet_n_rate():
+    def greet():
+        pname = input("Please Enter Your Name ")
+        print(f"Hello,{pname}! thanks for choosing us")
+        
+    def rating():
+        rating = int(input("please rate from 1-5 \n please enter your input in numbers"))
+        if rating > 5:
+            print("Invalid rating. Please rate between 1-5.")
+        else:
+            print(f"Thanks for rating us {rating}!")
+            
+    greet()
+    rating()
+
+greet_n_rate()
